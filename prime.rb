4a99d19cf3fc6aec5266cfa7ff78e7bce3fae179
Tomas Perez <tomasitopg6@gmail.com>
2020-06-19 02:08:23 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+
+def prime?(number)
+  
+  i = 2
+  
+  if number > 1
+    range = (i..number - 1).to_a
+    range.none? do | num |
+      number % num == 0
+    end
+  else
+    false
+  end
+  
+end
